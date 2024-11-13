c8bada121fcc4f9ffeba2e22ad7a9a8a70448a82
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-11-13 13:13:54 -0300

    feature: Função whitdraw

--- index.mjs
+++ index.mjs
@@ -29,7 +29,7 @@ function operation() {
         } else if(action === 'Consultar Saldo') {
             getAccountBalance()
         } else if(action === 'Sacar') {
-
+            whithdraw()
         } else if(action === 'Sair') {
             console.log(chalk.bgBlue.black('Obrigado por usar o Accounts!'))
             process.exit()
@@ -174,3 +174,60 @@ function getAccountBalance() {
     .catch(err => console.log(err))
 }
 
+function whithdraw() {
+
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Qual o nome da sua conta?'
+        }
+    ]).then((answer) => {
+
+        const accountName = answer['accountName']
+
+        if(!checkAccount(accountName)) {
+            return whithdraw()
+        }
+
+        inquirer.prompt([
+            {
+                name: 'amount',
+                message: 'Quanto você deseja sacar?'
+            }
+        ]).then((answer) => {
+
+            const amount = answer['amount']
+            removeAmount(accountName, amount)
+
+        })
+        .catch(err => console.log(err))
+
+    })
+    .catch(err => console.log(err))
+
+}
+
+function removeAmount(accountName, amount) {
+
+    const accountData = getAccount(accountName)
+
+    if(!amount) {
+        console.log(chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde'))
+        return whithdraw()
+    }
+
+    if(accountData.balance < amount) {
+        console.log(chalk.bgRed.black('Valor indisponível'))
+        return whithdraw()
+    }
+
+    accountData.balance = parseFloat(accountData.balance) - parseFloat(amount)
+
+    fs.writeFileSync(`accounts/${accountName}.json`, JSON.stringify(accountData), function(err) {
+        console.log(err)
+    })
+
+    console.log(chalk.green(`Foi realizado um saque de R$${amount} da sua conta!`))
+    operation()
+
+}
